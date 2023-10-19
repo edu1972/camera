706069ee97d9e639bb50329bcdc322885b4efa48
Luis Eduardo M. de Jesus <54754177+edu1972@users.noreply.github.com>
2023-10-19 15:19:27 -0300

    identação

--- camera.ino
+++ camera.ino
@@ -29,12 +29,7 @@ void loop() {
 	
 	else{
 		digitalWrite(13, LOW); //desliga o infravermelho
-			}
+	}
 }
 
-/*  
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(5000);                       // wait for a second
-*/
+
